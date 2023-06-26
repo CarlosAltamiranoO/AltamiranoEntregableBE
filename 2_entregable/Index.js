2c23d428b3773ea75ba987350d3308a5283861c3
Carlos Altamirano <carloscba.utn@gmail.com>
2023-06-26 01:23:42 -0300

    coreegir try and catch

--- 2_entregable/Index.js
+++ 2_entregable/Index.js
@@ -10,74 +10,117 @@ class ProductManager {
     }
 
     async getProduct() {
-        const json = await fs.promises.readFile(this.path, 'utf-8')
-        if (json === "") return "la lista esta vacia"
-        else {
-            this.products = JSON.parse(json)
+        try {
+            const json = await fs.promises.readFile(this.path, 'utf-8')
+            if (json === "") return "la lista esta vacia"
+            else {
+                this.products = JSON.parse(json)
+            }
+            return this.products
+
+        } catch (error) {
+            console.error(error)
         }
-        return this.products
+
     }
 
     async salveProduct() {
-        const json = JSON.stringify(this.products, null, 2)
-        await fs.promises.writeFile(this.path, json)
-        return "se guardo el producto"
+        try {
+            const json = JSON.stringify(this.products, null, 2)
+            await fs.promises.writeFile(this.path, json)
+            return "se guardo el producto"
+
+        } catch (error) {
+            console.error(error)
+
+        }
+
     }
 
     async addProduct(title, description, price, thumbnail, code, stock) {
-        await this.getProduct()
-        if (this.products.find((aux) => aux.code === code)) return console.log("el producto ya esta cargado");
-        else {
-            this.id++
-            this.products.push({ id: this.id, title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock })
+        try {
+            await this.getProduct()
+            if (this.products.find((aux) => aux.code === code)) return console.log("el producto ya esta cargado");
+            else {
+                this.id++
+                this.products.push({ id: this.id, title: title, description: description, price: price, thumbnail: thumbnail, code: code, stock: stock })
+            }
+            console.log(await this.salveProduct())
+
+        } catch (error) {
+            console.error(error)
         }
-        console.log(await this.salveProduct())
+
     }
 
     async getProductsById(id) {
-        await this.getProduct()
-        let respuesta = this.products.find((producto) => producto.id === id);
-        (respuesta === undefined) ? console.log("no se encuentra el producto") : console.log("producto encontrado: ", respuesta);
+        try {
+            await this.getProduct()
+            let respuesta = this.products.find((producto) => producto.id === id);
+            (respuesta === undefined) ? console.log("no se encuentra el producto") : console.log("producto encontrado: ", respuesta);
+
+        } catch (error) {
+            console.error(error)
+        }
+
     }
 
     async updateProduct(id, campoActualizar, cambio) {
-        await this.getProduct()
-        const indice = this.products.findIndex(aux => aux.id === id)
-        if (indice === -1) {
-            console.log("no hay producto a actualizar")
-            return
+        try {
+            await this.getProduct()
+            const indice = this.products.findIndex(aux => aux.id === id)
+            if (indice === -1) {
+                console.log("no hay producto a actualizar")
+                return
+            }
+            const producto = this.products[indice]
+            producto[campoActualizar] = cambio
+            this.products[indice] = producto
+            await this.salveProduct()
+            console.log("Producto actualizado:", producto)
+
+        } catch (error) {
+            console.log(error)
         }
-        const producto = this.products[indice]
-        producto[campoActualizar] = cambio
-        this.products[indice] = producto
-        await this.salveProduct()
-        console.log("Producto actualizado:", producto)
+
     }
 
     async deleteProduct(id) {
-        await this.getProduct()
-        const aux = this.products.filter((producto) => producto.id !== id)
-        if (JSON.stringify(aux) === JSON.stringify(this.products)) {
-            console.log("No hay producto a eliminar")
+        try {
+            await this.getProduct()
+            const aux = this.products.filter((producto) => producto.id !== id)
+            if (JSON.stringify(aux) === JSON.stringify(this.products)) {
+                console.log("No hay producto a eliminar")
+            }
+            else {
+                this.products = aux
+                await this.salveProduct()
+                console.log("producto eliminado")
+            }
+
+        } catch (error) {
+            console.log(error)
         }
-        else {
-            this.products = aux
-            await this.salveProduct()
-            console.log("producto eliminado")
-        }
+
     }
 }
 
 
 
 async function prueva(aux) {
-    await aux.addProduct("tablet", "tablet samsung", 100000, "Sin imagen", "a5", 20)
-    await aux.addProduct("PC", "NPC gamer", 200000, "Sin imagen", "a167", 16)
-    await aux.getProductsById(2)
-    await aux.addProduct("teclado", "teclado mecanico", 40000, "Sin imagen", "a166", 6)
-    await aux.updateProduct(2, "thumbnail", "con imagen en algun lado")
-    await aux.deleteProduct(1)
-    console.log("lista final: ", await aux.getProduct())
+    try {
+        await aux.addProduct("tablet", "tablet samsung", 100000, "Sin imagen", "a5", 20)
+        await aux.addProduct("PC", "NPC gamer", 200000, "Sin imagen", "a167", 16)
+        await aux.getProductsById(2)
+        await aux.addProduct("teclado", "teclado mecanico", 40000, "Sin imagen", "a166", 6)
+        await aux.updateProduct(2, "thumbnail", "con imagen en algun lado")
+        await aux.deleteProduct(1)
+        console.log("lista final: ", await aux.getProduct())
+
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 const agenda = new ProductManager('./Archivo.txt')
